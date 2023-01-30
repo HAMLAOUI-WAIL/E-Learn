3ca027ae2b0d973b8816641aa8917b1e23fbc40d
wailHAMLAOUI <123874882+wailHAMLAOUI@users.noreply.github.com>
2023-01-30 23:03:38 +0100

    Update urls.py

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -7,15 +7,15 @@ urlpatterns = [
     path('profile/<int:pk>',views.UserListDtail.as_view()),
     path('utilisateur/', views.UTILISATEURSList.as_view()),
     path('utilisateur/<int:pk>/', views.UTILISATEURSDetail.as_view()),
-    path('login',views.utilisateur_login),
+    path('login/',views.utilisateur_login),
     path('pages/',views.FlatPagesList.as_view()),
     path('pages/<int:pk>/<str:page_slug>/',views.FlatPagesDetail.as_view()),
     path('categorie/', views.CATEGORIESList.as_view()),
     path('annonce/', views.ANNONCESList.as_view()),
     
-    #path('annonces', views.annonces, name='annonces'),
+    # path('annonces/', views.annonce_list.as_view()),
     #path('annonces/<int:id>', views.annonce_detail)
-    #path('search-courses/<str:searchstring>', views.ANNONCESList.as_view()),
+    path('search-courses/<str:searchstring>', views.ANNONCESList.as_view()),
     # path('profile/create',views.CreateUsrView.as_view()),
     # path('profile/update/<int:pk>',views.UserViewSet.as_view({'get': 'list'}))
 ]
