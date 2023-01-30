2747bc00bedebd3172786fd8eb129901d6e66254
wailHAMLAOUI <123874882+wailHAMLAOUI@users.noreply.github.com>
2023-01-30 22:14:04 +0100

    Update urls.py

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -4,7 +4,15 @@ from . import views
 urlpatterns = [
     path('',views.getRoutes,name="routes"),
     path('profile/',views.UserList.as_view()),
-    path('profile/<int:pk>',views.UserListDtail().as_view()),
+    path('profile/<int:pk>',views.UserListDtail.as_view()),
+    path('utilisateur/', views.UTILISATEURSList.as_view()),
+    path('utilisateur/<int:pk>/', views.UTILISATEURSDetail.as_view()),
+    path('login',views.utilisateur_login),
+    path('pages/',views.FlatPagesList.as_view()),
+    path('pages/<int:pk>/<str:page_slug>/',views.FlatPagesDetail.as_view()),
+    path('categorie/', views.CATEGORIESList.as_view()),
+    path('annonce/', views.ANNONCESList.as_view()),
+    #path('search-courses/<str:searchstring>', views.ANNONCESList.as_view()),
     # path('profile/create',views.CreateUsrView.as_view()),
     # path('profile/update/<int:pk>',views.UserViewSet.as_view({'get': 'list'}))
 ]
