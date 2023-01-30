1088de88445d97787f2d23379e91ee194c25bdb6
wailHAMLAOUI <123874882+wailHAMLAOUI@users.noreply.github.com>
2023-01-30 22:19:01 +0100

    Delete serializers.py

--- back-end/myproject/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Annonce
-
-class AnnonceSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Annonce
-        fields = ['description', 'email', 'telephone','categorie','details','theme','lieu','lieu','file','tarif']
