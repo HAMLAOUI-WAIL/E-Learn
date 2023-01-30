43219be364c449f8b26136027d53d2685f8b6334
Debabzia alaa irada <100482368+iradadebabzia@users.noreply.github.com>
2023-01-30 15:10:42 +0100

    Update Ajouterannonce.js

--- frontend/app/src/componant/Ajouterannonce.js
+++ frontend/app/src/componant/Ajouterannonce.js
@@ -9,7 +9,7 @@ import Search from './header/Search';
 import Navbar from './header/Navbar';
 //import Autocomplete from "react-google-autocomplete"
 
-export default function Ajouterannonce(){
+export default function Ajouterannonce(props){
   const [details, setDetails] = useState('');
   const [theme, setTheme] = useState('');
   const [description, setDescription] = useState('');
@@ -27,7 +27,16 @@ export default function Ajouterannonce(){
     <Navbar/>
       <div className="contain"> 
         <div className="Form">
-        <Form>
+        <Form
+        onSubmit={(e) => {e.preventDefault();
+        setDetails('');
+        setTheme('');
+        setDescription('');
+        setLieu('');
+        setTarif('');
+        setFile('');
+        setTelephone('');
+        props.Ajouterannonce(details,theme,description,lieu,tarif,file,telephone);}}>
         <FormGroup>
           <Label for="Description">Description</Label>
           <Input type="textarea" id="Description" placeholder="Entrez une description pour votre annonce" 
