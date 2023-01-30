ce4bf2ddc84bbc0c223084ffde0537a1b7ce984b
wailHAMLAOUI <123874882+wailHAMLAOUI@users.noreply.github.com>
2023-01-30 23:04:51 +0100

    Update admin.py

--- back-end/api/admin.py
+++ back-end/api/admin.py
@@ -7,9 +7,8 @@ from .models import MODULES
 from .models import CATEGORIES
 from .models import MESSAGES
 from .models import ANNONCES
-from .models import Annonce
-
-admin.site.register(Annonce)
+# from .models import Annonce
+# admin.site.register(Annonce)
 admin.site.register(MESSAGES)
 admin.site.register(ANNONCES)
 admin.site.register(CATEGORIES)
