a21fa6f2107f82af7de6f9cacc14cbb294184400
wailHAMLAOUI <123874882+wailHAMLAOUI@users.noreply.github.com>
2023-01-30 22:25:44 +0100

    Update urls.py

--- back-end/api/urls.py
+++ back-end/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.getRoutes,name="routes"),
+#     path('',views.getRoutes,name="routes"),
     path('profile/',views.UserList.as_view()),
     path('profile/<int:pk>',views.UserListDtail.as_view()),
     path('utilisateur/', views.UTILISATEURSList.as_view()),
@@ -12,6 +12,9 @@ urlpatterns = [
     path('pages/<int:pk>/<str:page_slug>/',views.FlatPagesDetail.as_view()),
     path('categorie/', views.CATEGORIESList.as_view()),
     path('annonce/', views.ANNONCESList.as_view()),
+    
+    #path('annonces', views.annonces, name='annonces'),
+    #path('annonces/<int:id>', views.annonce_detail)
     #path('search-courses/<str:searchstring>', views.ANNONCESList.as_view()),
     # path('profile/create',views.CreateUsrView.as_view()),
     # path('profile/update/<int:pk>',views.UserViewSet.as_view({'get': 'list'}))
